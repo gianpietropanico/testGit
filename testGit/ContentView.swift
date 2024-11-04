78d43826d9a94528d6b136761d395553acd732ba
Matteo Romano <mleviromano@gmail.com>
2024-11-04 15:35:04 +0100

    test

--- testGit/ContentView.swift
+++ testGit/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, guuuuys!")
+                .foregroundStyle(.white)
         }
         .padding()
     }
