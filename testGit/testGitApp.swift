a34f31eaddebdded607daadad77415b09f090d93
Gianpietro Panico <panico.gianpietro@gmail.com>
2024-11-04 15:18:30 +0100

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by Gianpietro Panico on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
